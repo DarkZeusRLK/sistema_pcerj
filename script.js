b0726ca62b5ae3ea3d9590c90bcc3e278ee1ba56
João Pedro <fernandesjoaopedro622@gmail.com>
2025-12-19 00:18:20 -0300

    att

--- script.js
+++ script.js
@@ -13,13 +13,13 @@ const POSICOES = {
   // X = Horizontal (Esquerda/Direita)
   // Y = Vertical (Cima/Baixo)
 
-  nome: { x: 458, y: 279 },
-  id: { x: 754, y: 443 },
+  nome: { x: 458, y: 250 },
+  id: { x: 754, y: 433 },
 
   // Linha de baixo
-  rg: { x: 576, y: 443 },
-  expedicao: { x: 122, y: 451 }, // <--- NOVA POSIÇÃO (Ajuste se ficar em cima de outro)
-  validade: { x: 304, y: 451 },
+  rg: { x: 576, y: 433 },
+  expedicao: { x: 122, y: 435 }, // <--- NOVA POSIÇÃO (Ajuste se ficar em cima de outro)
+  validade: { x: 304, y: 435 },
 
   // Estilo da Fonte
   corTexto: "#000000",
